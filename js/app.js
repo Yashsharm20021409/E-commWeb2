38eaaa92fa4b0a5b2a9a6ce70eec7d7baaf714a0
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-10-09 17:57:31 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
-const bar = document.getElementsByClassName('megamenu');
-const cls = document.getElementsByClassName('megamenu-list');
-const open = document.getElementsByClassName('megaMenuOpen');
-
-if(bar){
-    open.addEventListener('click', ()=>{
-        open.classList.add('Open');
-    })
-}
+const open = document.getElementById('openMenu');
+const close = document.getElementById('closeMenu');
+const list = document.getElementById('list');
 
 if(open){
     open.addEventListener('click', ()=>{
+        list.classList.add('active');
+    });
+}
+
+if(close){
+    close.addEventListener('click', ()=>{
         list.classList.remove('active');
     })
 }
+
+
+// mega menu
+
+const openList = document.getElementById('openMegaMenu');
+const foc = document.getElementById('megaMenu')
+
+openList.addEventListener('click' , ()=>{
+    foc.classList.add('activeMenu');
+});
