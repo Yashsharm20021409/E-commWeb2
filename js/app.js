b4a1dfde02e39d2768c241a254650163e202af7d
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-11-04 16:46:03 +0530

    update

--- js/app.js
+++ js/app.js
@@ -29,15 +29,30 @@ const MegaMenu = document.getElementById('mm1');
 
 if (On) {
     On.addEventListener('click', () => {
+
+        // Mens open
+
         MegaMenu.classList.add('megaMenuJs');
         pad.classList.add('padd');
         console.log("Added");
+
+        // Shop close
+        MegaMenu2.classList.remove('megaMenuJs');
+        pad2.classList.remove('padd');
+        console.log("removed");
+
+        // Women close
+
+        MegaMenu1.classList.remove('megaMenuJs');
+        pad1.classList.remove('padd');
+        console.log("removed");
     });
 }
 
 if (closeMega) {
     closeMega.addEventListener('click', () => {
-        MegaMenu.classList.add('megaMenuJsremove');
+        // MegaMenu.classList.add('megaMenuJsremove');
+        MegaMenu.classList.remove('megaMenuJs');
         pad.classList.remove('padd');
         console.log("removed");
     });
@@ -51,15 +66,32 @@ const MegaMenu1 = document.getElementById('mm2');
 
 if (On1) {
     On1.addEventListener('click', () => {
+
+        // Women Open
+
+
         MegaMenu1.classList.add('megaMenuJs');
         pad1.classList.add('padd');
         console.log("Added");
+
+        // Men close
+        MegaMenu.classList.remove('megaMenuJs');
+        pad.classList.remove('padd');
+        console.log("removed");
+
+        // shop close
+
+        MegaMenu2.classList.remove('megaMenuJs');
+        pad2.classList.remove('padd');
+        console.log("removed");
+
     });
 }
 
 if (closeMega2) {
     closeMega2.addEventListener('click', () => {
-        MegaMenu1.classList.add('megaMenuJsremove');
+        // MegaMenu1.classList.add('megaMenuJsremove');
+        MegaMenu1.classList.remove('megaMenuJs');
         pad1.classList.remove('padd');
         console.log("removed");
     });
@@ -72,15 +104,31 @@ const MegaMenu2 = document.getElementById('mm3');
 
 if (On2) {
     On2.addEventListener('click', () => {
+
+        // Shop open
+
         MegaMenu2.classList.add('megaMenuJs');
         pad2.classList.add('padd');
         console.log("Added");
+        // men close
+        MegaMenu.classList.remove('megaMenuJs');
+        pad.classList.remove('padd');
+        console.log("removed");
+
+        // Women close
+
+        MegaMenu1.classList.remove('megaMenuJs');
+        pad1.classList.remove('padd');
+        console.log("removed");
+
+
     });
 }
 
 if (closeMega3) {
     closeMega3.addEventListener('click', () => {
-        MegaMenu2.classList.add('megaMenuJsremove');
+        // MegaMenu2.classList.add('megaMenuJsremove');
+        MegaMenu2.classList.remove('megaMenuJs');
         pad2.classList.remove('padd');
         console.log("removed");
     });
